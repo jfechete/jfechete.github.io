88c455d6eed78906787b0e10ddd58978b0dd7703
jfechete <60266095+jfechete@users.noreply.github.com>
2024-07-23 23:23:33 -0700

    Improved second page

--- portfolioNavigation.js
+++ portfolioNavigation.js
@@ -6,13 +6,15 @@ function updateArrows() {
     }
     let portfolio = document.getElementById("main-portfolio");
     let portfolioRect = portfolio.getBoundingClientRect();
-    let yDiff = Math.abs(portfolioRect.top/portfolioRect.height);
-    let pxOffset = 10-20*yDiff;
+    let floatPos = Math.abs(portfolioRect.top/portfolioRect.height);
+    floatPos = Math.max(0, floatPos - 0.25);
+    floatPos = floatPos * 2;
+    let pxOffset = 10-20*floatPos;
 
     let leftArrow = document.getElementById("left-portfolio-arrow");
     let rightArrow = document.getElementById("right-portfolio-arrow");
-    leftArrow.style.left = pxOffset - yDiff*leftArrow.offsetWidth + "px";
-    rightArrow.style.right = pxOffset - yDiff*rightArrow.offsetWidth + "px";
+    leftArrow.style.left = pxOffset - floatPos*leftArrow.offsetWidth + "px";
+    rightArrow.style.right = pxOffset - floatPos*rightArrow.offsetWidth + "px";
 }
 
 const SPRING_FORCE = 0.2;
@@ -25,11 +27,15 @@ function updateVideos() {
         return;
     }
 
+    let normalLeft = 10;
+    if (screen.width <= screen.height) {
+        normalLeft = 2.5;
+    }
     currentOffset += SPRING_FORCE*(desiredOffset-currentOffset);
     for (let i = 0; i < videos.length; i++) {
         let vidPos = mod(i+currentOffset, videos.length);
-        videos[i].style.left = (vidPos-1)*100 + 10 + "vw";
-        videos[i].style.opacity = Math.max(0, 
+        videos[i].style.left = (vidPos-1)*100 + normalLeft + "vw";
+        videos[i].style.opacity = Math.max(0,
             -Math.abs(vidPos-1)+1
         );
     }
@@ -46,7 +52,7 @@ function prevVid() {
 }
 
 function pauseVideos() {
-    let videos = document.getElementsByClassName("portfolio-vid");
+    let videos = document.querySelectorAll(".portfolio-div>div>video");
     for (let i = 0; i < videos.length; i++) {
         if (!videos[i].paused) {
             videos[i].pause();
